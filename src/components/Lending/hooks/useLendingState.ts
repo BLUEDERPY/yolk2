f47dafe4b1f6c1bd10154cc03828999d207829f6
BLUEDERPY <74120282+BLUEDERPY@users.noreply.github.com>
2025-08-01 11:34:48 -0400

    Updated useLendingState.ts

--- src/components/Lending/hooks/useLendingState.ts
+++ src/components/Lending/hooks/useLendingState.ts
@@ -27,8 +27,8 @@ export const useLendingState = (tokenType: 'eggs' | 'yolk' | 'nest' = 'eggs') =>
   const loan = userData[tokenType].loan;
   const balance = userData[tokenType].balance;
   
-  const borrowed = loan ? loan?.collateral | BigInt(0) : undefined;
-  const collateral = loan ? loan?.borrowed | BigInt(0): undefined;
+  const borrowed = loan ? parseEther(loan?.collateral) : undefined;
+  const collateral = loan ? parseEther(loan?.borrowed) : undefined;
   const minDuration = useMemo(() => {
     if (borrowed) return dateDiff(new Date(Number(loan[2]) * 1000), new Date());
   }, [borrowed, loan]);
