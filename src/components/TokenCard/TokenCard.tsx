4055e3b0edf10c76f5c89f0b445eb5bf28464c02
BLUEDERPY <74120282+BLUEDERPY@users.noreply.github.com>
2025-08-01 12:19:22 -0400

    Updated TokenCard.tsx

--- src/components/TokenCard/TokenCard.tsx
+++ src/components/TokenCard/TokenCard.tsx
@@ -314,6 +314,7 @@ export const TokenCard: React.FC<TokenCardProps> = ({
                   </Box>
                   
                   <SwapInput
+                     tokenType={tokenType}
                     label={tradeDirection === "buy" ? tokenConfig.backingTitle : tokenConfig.tokenName}
                     value={tradeAmount}
                     onChange={setTradeAmount}
